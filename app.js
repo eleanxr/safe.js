5515dce3c76259d377e65f14ce7ddc7d01291886
Will Dicharry <wdicharry@gmail.com>
2013-11-30 15:36:57 -0700

    Add mongodb database.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
-express = require('express')
-routes = require('./routes')
+var express = require('express')
+var mongoose = require('mongoose')
+var routes = require('./routes')
+var user = require('./models/user')
 
-app = express()
+var app = express()
 
 // Middleware
 
@@ -12,7 +14,33 @@ app.get('/login', routes.login);
 
 // REST API routes
 
-var port = 8000;
-app.listen(port);
-console.log('SafeServer started on port ' + port);
+function start_server() {
+  var port = 8000;
+  console.log('Starting server on port ' + port);
+  app.listen(port);
+  console.log('SafeServer started');
+}
 
+// Connect to the database.
+console.log('Connecting to MongoDB instance')
+mongoose.connect('mongodb://localhost/test')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'database connection error:'))
+db.once('open', function(){
+  var User = user.User;
+  User.find({username: 'admin'}, function(err, admins){
+    if (err) {
+      console.error("Failed to query for administrator");
+    } else {
+      if (admins.length == 0) {
+        console.log("No admin user detected, creating default");
+        admin = new User({username: 'admin', password: 'admin'});
+        admin.save();
+      } else {
+        admin = admins[0];
+        console.log('Found administrative user');
+      }
+    }
+  });
+  start_server();
+});
